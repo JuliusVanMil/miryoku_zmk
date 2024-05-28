46f76698fe8a6a593db99a73f4766abe7db58524
Julius van Mil <j.van.mil@itandcare.nl>
2024-05-28 17:41:43 +0200

    Added symbols to left hand of NUM layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -8,9 +8,9 @@ U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp 
 U_NP,              U_NP,              U_LT(U_FUN, ESC),  U_LT(U_NUM, BSPC), U_LT(U_SYM, TAB),  U_LT(U_MOUSE, RET),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, DEL),U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp PLUS,          &kp N7,            &kp N8,            &kp N9,            &kp GRAVE,         \
+&kp EXCL,          &kp AT,            &kp HASH,          &kp DLLR,          &kp PRCNT,         &kp PLUS,          &kp N7,            &kp N8,            &kp N9,            &kp GRAVE,         \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp EQUAL,         &kp N4,            &kp N5,            &kp N6,            &kp SEMI,          \
-U_NA,              &kp RALT,          &u_to_U_NAV,       &u_to_U_NUM,       U_NA,              &kp BSLH,          &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          \
+&kp CARET,         &kp AMPS,          &kp ASTRK,         &kp MINUS,         &kp EQUAL,         &kp BSLH,          &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp MINUS,         &kp N0,            &kp DOT,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
