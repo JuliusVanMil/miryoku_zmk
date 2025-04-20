597ff35ecac65ac85a29a3f9774712f321fb905d
Julius van Mil <32191035+JuliusVanMil@users.noreply.github.com>
2025-04-20 23:35:13 +0200

    timeless home-row mods modification

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,9 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+//                                                                                      LEFT | RIGHT
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_HML(LGUI, A),    U_HML(LALT, R),    U_HML(LCTRL, S),   U_HML(LSHFT, T),   &kp G,             &kp M,             U_HMR(LSHFT, N),   U_HMR(LCTRL, E),   U_HMR(LALT, I),    U_HMR(LGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_FUN, ESC),  U_LT(U_NUM, BSPC), U_LT(U_SYM, TAB),  U_LT(U_MOUSE, RET),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, DEL),U_NP,              U_NP
 
@@ -16,5 +17,5 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 #define MIRYOKU_LAYER_SYM \
 &kp EXCL,          &kp AT,            &kp HASH,          &kp DLLR,          &kp PRCNT,         &kp LBKT,          &kp LT,            &kp EQUAL,         &kp GT,            &kp RBKT,          \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LBRC,          &kp LPAR,          &kp COLON,         &kp RPAR,          &kp RBRC,          \
-&kp CARET,         &kp AMPS,          &kp ASTRK,         &kp MINUS,         &kp EQUAL,         &kp EXCL,          &kp EQUAL,         &kp AMPS,          &kp PIPE,          &kp SEMI,         \
+&kp CARET,         &kp AMPS,          &kp ASTRK,         &kp MINUS,         &kp EQUAL,         &kp EXCL,          &kp EQUAL,         &kp AMPS,          &kp PIPE,          &kp SEMI,          \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp MINUS,         &kp UNDER,         &kp SEMI,          U_NP,              U_NP
